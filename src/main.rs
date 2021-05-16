469ceacd0990688ba9927d5678942869d94af035
Fady Adal <2masadel@gmail.com>
2021-05-16 07:48:42 +0200

    fixed no shebang bugs and added tests

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,20 @@ impl<'a> SFile<'a> {
         let mut rdr = BufReader::new(&f);
         let mut possible_shebang = String::new();
         rdr.read_line(&mut possible_shebang)?;
+        let mut rest = String::new();
+        rdr.read_to_string(&mut rest)?;
+
+        let mut contents = String::new();
 
         let shebang = if possible_shebang.starts_with("#!") {
             Some(possible_shebang)
         } else {
+            contents.push_str(possible_shebang.as_str());
             None
         };
+        contents.push_str(rest.as_str());
 
         let mode = fs::metadata(path)?.permissions().mode();
-
-        let mut contents = String::new();
-        rdr.read_to_string(&mut contents)?;
 
         Ok(Self {
             _descriptor: f,
@@ -75,11 +78,74 @@ impl<'a> SFile<'a> {
     fn write(&mut self) {}
 }
 
-fn main() {
-    let mut sfile = SFile::new(Path::new("/home/fadal/hello.py")).expect("Fucking erred.");
+fn main() {}
 
-    println!("{:?}", sfile.shebang());
-    println!("{:?}", sfile.contents());
-    sfile.make_executable().expect("Making exec fucking erred.");
-    println!("{:?}", sfile.executable());
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::prelude::*;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn sfile_picks_up_shebang_contents_and_executability() -> Result<(), std::io::Error> {
+        let tmpfile = NamedTempFile::new()?;
+        write!(
+            tmpfile.as_file(),
+            "#!/usr/bin/python\n\nprint(\"Hello, World!\")"
+        )?;
+
+        let tmppath = tmpfile.into_temp_path();
+
+        let sfile = SFile::new(&tmppath)?;
+        assert_eq!(sfile.shebang(), &Some(String::from("#!/usr/bin/python\n")));
+        assert_eq!(sfile.executable(), false);
+        assert_eq!(
+            sfile.contents(),
+            &String::from("\nprint(\"Hello, World!\")")
+        );
+
+        tmppath.close()?;
+        Ok(())
+    }
+
+    #[test]
+    fn sfile_no_shebang() -> Result<(), std::io::Error> {
+        let tmpfile = NamedTempFile::new()?;
+        write!(
+            tmpfile.as_file(),
+            "This is a regular file.\n\nNo shebang is here.\n"
+        )?;
+
+        let tmppath = tmpfile.into_temp_path();
+
+        let sfile = SFile::new(&tmppath)?;
+        assert_eq!(sfile.shebang(), &None);
+        assert_eq!(sfile.executable(), false);
+        assert_eq!(
+            sfile.contents(),
+            &String::from("This is a regular file.\n\nNo shebang is here.\n")
+        );
+
+        tmppath.close()?;
+        Ok(())
+    }
+
+    #[test]
+    fn make_shabang_works() -> Result<(), std::io::Error> {
+        let tmpfile = NamedTempFile::new()?;
+        write!(
+            tmpfile.as_file(),
+            "#!/usr/bin/python\n\nprint(\"Hello, World!\")"
+        )?;
+
+        let tmppath = tmpfile.into_temp_path();
+
+        let mut sfile = SFile::new(&tmppath)?;
+        assert_eq!(sfile.executable(), false);
+        sfile.make_executable()?;
+        assert_eq!(sfile.executable(), true);
+
+        tmppath.close()?;
+        Ok(())
+    }
 }
